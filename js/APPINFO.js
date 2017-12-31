55f693096b548ba5537b60df2b9ddf42f37a472b
liuxueke <xueke.liu@outlook.com>
2017-12-31 19:30:35 +0800

    Details: 	1. Ordinary users hide some of the features. 	2. fix update function. 	3. page adjustment.

--- js/APPINFO.js
+++ js/APPINFO.js
@@ -1,4 +1,4 @@
-/*方便内网调试配置全局变量*/
+/*APP相关信息*/
 var APPINFO = new Object();
-	APPINFO.version = "1.0.0";
-	APPINFO.serverURL = "http://192.168.1.139";
+	APPINFO.version = "1.0.0";					//APP版本
+	APPINFO.serverURL = "http://192.168.1.139";	//服务器地址
