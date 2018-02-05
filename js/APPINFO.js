c1b9f42ddc1f9819e471d4b1c6a063b6d070990f
zx48822821 <752138572@qq.com>
2018-02-05 08:29:29 +0800

    Details: 	1.CSS modified.

--- js/APPINFO.js
+++ js/APPINFO.js
@@ -3,3 +3,4 @@ var APPINFO = new Object();
 	APPINFO.version = "1.0.0";					//APP版本
 	APPINFO.serverURL = "http://192.168.1.139";	//服务器地址
 	//APPINFO.serverURL = "http://192.168.0.135:88";	//OA服务器地址
+	//APPINFO.serverURL = "http://192.168.0.100";	//服务器地址2
